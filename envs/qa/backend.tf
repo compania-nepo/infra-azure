daebb725904792997556f1de25bb45f14d61705f
arturo24 <39010207+arturo24@users.noreply.github.com>
2026-05-30 22:18:38 -0500

    Add files via upload

--- envs/qa/backend.tf
+++ envs/qa/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-cicd-terraform-app-apellido" # Reemplazar por apellido
-    storage_account_name = "tfstateapellido"                # Reemplazar por apellido
+    resource_group_name  = "rg-cicd-terraform-app-nepo" # Reemplazar por nepo
+    storage_account_name = "tfstatenepo"                # Reemplazar por nepo
     container_name       = "tfstate"
     key                  = "qa/terraform.tfstate"
   }
